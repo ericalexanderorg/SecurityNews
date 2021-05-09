dbb6c7b4fb8d296da0f867d71cda37d767f14742
ericalexanderorg <ericalexanderorg@users.noreply.github.com>
2021-05-09 04:09:38 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.a90c13e4091ef05b5030699f6604deb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2795e30fdddf0550996a386979ad8e41",
+    "url": "/SecurityNews/index.html"
+  },
+  {
+    "revision": "534c6aa5787d9c868aff",
+    "url": "/SecurityNews/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "710c73b0728de6eb3dd7",
+    "url": "/SecurityNews/static/js/2.2f88fedc.chunk.js"
+  },
+  {
+    "revision": "5d9ac27b92c76c236f4eb7f51c0f90e4",
+    "url": "/SecurityNews/static/js/2.2f88fedc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "534c6aa5787d9c868aff",
+    "url": "/SecurityNews/static/js/main.2a9a45d6.chunk.js"
+  },
+  {
+    "revision": "364443b8807f62abd664",
+    "url": "/SecurityNews/static/js/runtime-main.e0748928.js"
+  }
+]);
